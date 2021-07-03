37d587c5c16cb916b1bb92052184aa2367fc9375
oyyk <745691616@qq.com>
2021-07-03 09:54:12 +0800

    增加自定义配置项示例

--- src/main/java/com/oyyk/homepage/controller/TestController.java
+++ src/main/java/com/oyyk/homepage/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.oyyk.homepage.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,12 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    @Value("${test.hello}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello" + testHello;
     }
 
     @PostMapping("/hello/post")
