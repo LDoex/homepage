028d2d0fd07d1af28d45cbf9edf4514b7b5864ca
oyyk <745691616@qq.com>
2021-07-03 09:33:05 +0800

    增加测试接口，修改componentscan扫描位置

--- src/main/java/com/oyyk/homepage/config/HomepageApplication.java
+++ src/main/java/com/oyyk/homepage/config/HomepageApplication.java
@@ -1,11 +1,13 @@
-package com.oyyk.homepage;
+package com.oyyk.homepage.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.oyyk")
 @SpringBootApplication
 public class HomepageApplication {
     private static final Logger LOG = LoggerFactory.getLogger(HomepageApplication.class);
