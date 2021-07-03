c9e615976e352de31e32c3e2682ba744fe1af5cb
oyyk <745691616@qq.com>
2021-07-03 09:45:00 +0800

    增加httpTest

--- src/main/java/com/oyyk/homepage/controller/TestController.java
+++ src/main/java/com/oyyk/homepage/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.oyyk.homepage.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 
 @RestController
@@ -10,5 +13,10 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello!post"+name;
     }
 }
