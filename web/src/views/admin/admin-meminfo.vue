8424f6190b025cb4b8340d6554eaa59a9317ca82
oyyk <745691616@qq.com>
2021-07-06 15:54:15 +0800

    增加前后端分页

--- web/src/views/admin/admin-meminfo.vue
+++ web/src/views/admin/admin-meminfo.vue
@@ -98,7 +98,12 @@ export default defineComponent({
      **/
     const handleQuery = (params: any) => {
       loading.value = true;
-      axios.get("/memberinfo/list", params).then((response)=>{
+      axios.get("/memberinfo/list", {
+        params: {
+          page: params.page,
+          size: params.size
+        }
+      }).then((response)=>{
         loading.value = false;
         const data = response.data;
         if(data.success){
@@ -108,6 +113,7 @@ export default defineComponent({
         }
         //重置分页
         pagination.value.current = params.page;
+        pagination.value.total = data.content.total;
       });
     }
 
@@ -132,7 +138,10 @@ export default defineComponent({
 
 
     onMounted(()=>{
-      handleQuery({});
+      handleQuery({
+        page: 1,
+        size: pagination.value.pageSize,
+      });
     })
 
     return {
