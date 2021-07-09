89cc1d4bb18ee0d6a522898abc8acd86c2f78435
oyyk <745691616@qq.com>
2021-07-09 12:31:59 +0800

    集成wangeditor@4.6.3，增加页面内容表，增加持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -75,3 +75,13 @@ insert into `doc` (id, outCate_id, parent, name, sort, view_count, vote_count) v
 insert into `doc` (id, outCate_id, parent, name, sort, view_count, vote_count) values (4, 1, 3, '文档2.1', 1, 0, 0);
 insert into `doc` (id, outCate_id, parent, name, sort, view_count, vote_count) values (5, 1, 3, '文档2.2', 2, 0, 0);
 insert into `doc` (id, outCate_id, parent, name, sort, view_count, vote_count) values (6, 1, 5, '文档2.2.1', 1, 0, 0);
+
+
+-- 页面内容
+drop table if exists `content`;
+create table `content`(
+                          `id` bigint not null comment 'id',
+                          `content` mediumtext not null comment '内容',
+                          primary key (`id`)
+
+)engine=innodb default charset=utf8mb4 comment='页面内容';
