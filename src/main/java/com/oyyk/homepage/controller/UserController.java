bfedbbfa6d9a83a3fcc785d3f38dd8b7e386b96b
oyyk <745691616@qq.com>
2021-07-11 22:10:25 +0800

    增加用户接口代码，待调试

---
+++ src/main/java/com/oyyk/homepage/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.oyyk.homepage.controller;
+
+import com.oyyk.homepage.req.UserQueryReq;
+import com.oyyk.homepage.req.UserSaveReq;
+import com.oyyk.homepage.resp.CommonResp;
+import com.oyyk.homepage.resp.UserQueryResp;
+import com.oyyk.homepage.resp.PageResp;
+import com.oyyk.homepage.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    @Resource
+    private UserService userService;
+
+    @RequestMapping("/list")
+    public CommonResp list(@Valid UserQueryReq req){
+        CommonResp<PageResp<UserQueryResp>> resp = new CommonResp<>();
+        PageResp<UserQueryResp> list = userService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody UserSaveReq req){
+        CommonResp resp = new CommonResp<>();
+        userService.save(req);
+        return resp;
+    }
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id){
+        CommonResp resp = new CommonResp<>();
+        userService.delete(id);
+        return resp;
+    }
+}
