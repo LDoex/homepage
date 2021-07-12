68453f4045f033f05e74b2e38797c3b7286a48b4
oyyk <745691616@qq.com>
2021-07-12 11:09:20 +0800

    密码加密传输和存储

--- web/src/views/admin/admin-user.vue
+++ web/src/views/admin/admin-user.vue
@@ -81,6 +81,9 @@ import axios from 'axios';
 import { message } from 'ant-design-vue';
 import {Tool} from '@/util/tool';
 
+declare let hexMd5: any;
+declare let KEY: any;
+
 
 export default defineComponent({
   setup() {
@@ -179,6 +182,7 @@ export default defineComponent({
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
+      userItem.value.password = hexMd5(userItem.value.password + KEY);
       axios.post("/user/save", userItem.value).then((response)=>{
         modalLoading.value = false;
         const data = response.data;
