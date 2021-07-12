b25991447d7cdbc283f05a8e87fe0066ea32b40a
oyyk <745691616@qq.com>
2021-07-12 20:36:07 +0800

    增加路由拦截

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -6,6 +6,8 @@ import AdminMemberinfo from '../views/admin/admin-meminfo.vue'
 import AdminHomeCategory from '../views/admin/admin-homeCategory.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
 import Doc from '../views/doc.vue'
+import store from "@/store";
+import {Tool} from "@/util/tool";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -21,22 +23,34 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/admin/user',
     name: 'AdminUser',
-    component: AdminUser
+    component: AdminUser,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/memberinfo',
     name: 'AdminMemberinfo',
-    component: AdminMemberinfo
+    component: AdminMemberinfo,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/homeCategory',
     name: 'AdminHomeCategory',
-    component: AdminHomeCategory
+    component: AdminHomeCategory,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/doc',
     name: 'AdminDoc',
-    component: AdminDoc
+    component: AdminDoc,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/about',
@@ -53,5 +67,24 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验：", item.meta.loginRequire);
+    return item.meta.loginRequire
+  })) {
+    const loginUser = store.state.user;
+    if (Tool.isEmpty(loginUser)) {
+      console.log("用户未登录！");
+      next('/');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
